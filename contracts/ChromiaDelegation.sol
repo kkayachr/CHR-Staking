93ab783feeb07e6959429b2cc4156d9851fb746b
Koray Kaya <koray.kaya@chromaway.com>
2023-02-08 12:13:21 +0100

    verify accumulated days added

--- contracts/ChromiaDelegation.sol
+++ contracts/ChromiaDelegation.sol
@@ -8,6 +8,7 @@ pragma solidity ^0.8.17;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import './TwoWeeksNoticeProvider.sol';
+import 'hardhat/console.sol';
 
 interface TwoWeeksNotice {
     function estimateAccumulated(address account) external view returns (uint128, uint128);
@@ -51,30 +52,23 @@ contract ChromiaDelegation is TwoWeeksNoticeProvider {
     }
 
     function verifyRemoteAccumulated(uint128 remoteAccumulated, address account) public view {
-        uint128 localAccumulated;
-        for (uint256 i = 0; i < delegations[account].delegationTimeline.length; i++) {
-            if (delegations[account].delegationTimeline[i].timePoint > delegations[account].processedDate) {
-                uint128 prevTimepoint = (delegations[account].processedDate >
-                    delegations[account].delegationTimeline[i - 1].timePoint)
-                    ? delegations[account].processedDate
-                    : delegations[account].delegationTimeline[i - 1].timePoint;
-                uint128 currentTimepoint = delegations[account].delegationTimeline[i].timePoint;
-                uint128 prevBalance = delegations[account].delegationTimeline[i - 1].balance;
-                localAccumulated += (currentTimepoint - prevTimepoint) * prevBalance;
-            }
-        }
-        localAccumulated +=
-            (delegations[account].delegationTimeline[delegations[account].delegationTimeline.length - 1].timePoint -
-                uint128(block.timestamp)) *
-            delegations[account].delegationTimeline[delegations[account].delegationTimeline.length - 1].balance;
-        require(localAccumulated == remoteAccumulated, 'Accumulated doesnt match with TWN');
+        uint128 localAccumulated = estimateYieldFrom(account, delegations[account].processedDate) / 86400;
+
+        require(
+            localAccumulated > (remoteAccumulated - delegations[account].processed) - 10,
+            'Accumulated doesnt match with TWN'
+        );
+        require(
+            localAccumulated < (remoteAccumulated - delegations[account].processed) + 10,
+            'Accumulated doesnt match with TWN'
+        );
     }
 
     function estimateYield(address account) public view returns (uint128 reward) {
         uint128 prevPaid = delegations[account].processed;
         (uint128 acc, ) = twn.estimateAccumulated(account);
-        verifyRemoteAccumulated(acc, account);
         if (acc > prevPaid) {
+            verifyRemoteAccumulated(acc, account);
             StakeState memory providerState = _states[getCurrentDelegation(account).delegatedTo];
             uint128 providerStakeEnd;
             for (uint256 i = 0; i < providerState.stakeTimeline.length; i++) {
@@ -88,7 +82,7 @@ contract ChromiaDelegation is TwoWeeksNoticeProvider {
             }
             uint128 subtractedYield;
             if (providerStakeEnd > 0) {
-                subtractedYield = calculateYieldOverflow(account, providerStakeEnd); // If provider has ended stake, subtract from that point
+                subtractedYield = estimateYieldFrom(account, providerStakeEnd); // If provider has ended stake, subtract from that point
             }
             uint128 delta = acc - prevPaid - subtractedYield;
             reward = (rewardPerDayPerToken * delta) / 1000000;
@@ -96,14 +90,14 @@ contract ChromiaDelegation is TwoWeeksNoticeProvider {
     }
 
     // If provider has stopped providing, we want to subtract "accumulated token days" from that point.
-    function calculateYieldOverflow(address account, uint128 providerStakeEnd) private view returns (uint128 subtractedYield) {
+    function estimateYieldFrom(address account, uint128 from) private view returns (uint128 subtractedYield) {
         uint128 prevTimepoint;
         uint128 deltaTime;
         if (delegations[account].delegationTimeline.length > 1) {
             for (uint256 i = 0; i < delegations[account].delegationTimeline.length; i++) {
-                if (delegations[account].delegationTimeline[i].timePoint > providerStakeEnd) {
-                    prevTimepoint = (providerStakeEnd > delegations[account].delegationTimeline[i - 1].timePoint)
-                        ? providerStakeEnd
+                if (delegations[account].delegationTimeline[i].timePoint > from) {
+                    prevTimepoint = (from > delegations[account].delegationTimeline[i - 1].timePoint)
+                        ? from
                         : delegations[account].delegationTimeline[i - 1].timePoint;
 
                     deltaTime = delegations[account].delegationTimeline[i].timePoint - prevTimepoint;
@@ -111,9 +105,9 @@ contract ChromiaDelegation is TwoWeeksNoticeProvider {
                 }
             }
         }
-        prevTimepoint = (providerStakeEnd >
+        prevTimepoint = (from >
             delegations[account].delegationTimeline[delegations[account].delegationTimeline.length - 1].timePoint)
-            ? providerStakeEnd
+            ? from
             : delegations[account].delegationTimeline[delegations[account].delegationTimeline.length - 1].timePoint;
 
         deltaTime = uint128(block.timestamp) - prevTimepoint;
@@ -147,7 +141,6 @@ contract ChromiaDelegation is TwoWeeksNoticeProvider {
         (uint64 delegateAmount, , , ) = twn.getStakeState(msg.sender);
         require(delegateAmount > 0, 'Must have a stake to delegate');
         userDelegation.processed = acc;
-        delegations[msg.sender].delegationTimeline[delegations[msg.sender].delegationTimeline.length - 1].delegatedTo = to;
         userDelegation.delegationTimeline.push(DelegationChange(uint128(block.timestamp), delegateAmount, to));
     }
 
