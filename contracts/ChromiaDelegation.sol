0150459a3d2f50bba64f913e6d91e740afd75497
Koray Kaya <koray.kaya@chromaway.com>
2023-02-14 13:44:13 +0100

    Seperated syncer/delegatorstate to its own contract for clarity

--- contracts/ChromiaDelegation.sol
+++ contracts/ChromiaDelegation.sol
@@ -8,36 +8,15 @@ pragma solidity ^0.8.17;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import './TwoWeeksNoticeProvider.sol';
+import './ChromiaDelegationSync.sol';
 import 'hardhat/console.sol';
 
-interface TwoWeeksNotice {
-    function estimateAccumulated(address account) external view returns (uint128, uint128);
-
-    function getStakeState(address account) external view returns (uint64, uint64, uint64, uint64);
-}
-
-contract ChromiaDelegation is TwoWeeksNoticeProvider {
-    TwoWeeksNotice public twn;
-
-    struct DelegationChange {
-        uint128 timePoint;
-        uint128 balance;
-        address delegatedTo;
-    }
-
+contract ChromiaDelegation is TwoWeeksNoticeProvider, ChromiaDelegationSync {
     struct RewardPerDayPerTokenChange {
         uint128 timePoint;
         uint128 rewardPerDayPerToken;
     }
 
-    struct DelegationState {
-        uint128 processed;
-        uint128 processedDate;
-        DelegationChange[] delegationChanges;
-        mapping(uint32 => DelegationChange) delegationTimeline; // each uint key is a week starting from "startTime"
-    }
-
-    mapping(address => DelegationState) public delegations;
     mapping(uint32 => RewardPerDayPerTokenChange) public rewardPerDayPerTokenTimeline;
 
     constructor(
@@ -46,7 +25,7 @@ contract ChromiaDelegation is TwoWeeksNoticeProvider {
         address _owner,
         uint64 inital_reward,
         uint64 inital_reward_provider
-    ) TwoWeeksNoticeProvider(_token, _owner, inital_reward_provider) {
+    ) TwoWeeksNoticeProvider(_token, _owner, inital_reward_provider) ChromiaDelegationSync(_twn) {
         twn = _twn;
         rewardPerDayPerTokenTimeline[0] = RewardPerDayPerTokenChange(uint128(block.timestamp), inital_reward);
     }
@@ -55,24 +34,6 @@ contract ChromiaDelegation is TwoWeeksNoticeProvider {
         require(msg.sender == owner);
         uint32 currentEpoch = getCurrentEpoch();
         rewardPerDayPerTokenTimeline[currentEpoch + 1] = RewardPerDayPerTokenChange(getEpochTime(currentEpoch + 1), rewardRate);
-    }
-
-    function verifyRemoteAccumulated(address account) public view {
-        (uint128 remoteAccumulated, ) = twn.estimateAccumulated(account);
-        uint128 localAccumulated = estimateAccumulatedFromTo(
-            account,
-            delegations[account].processedDate,
-            uint128(block.timestamp)
-        );
-
-        require(
-            localAccumulated > (remoteAccumulated - delegations[account].processed) - 10,
-            'Accumulated doesnt match with TWN'
-        );
-        require(
-            localAccumulated < (remoteAccumulated - delegations[account].processed) + 10,
-            'Accumulated doesnt match with TWN'
-        );
     }
 
     function getActiveDelegation(address account, uint32 epoch) public view returns (DelegationChange memory activeDelegation) {
@@ -129,33 +90,6 @@ contract ChromiaDelegation is TwoWeeksNoticeProvider {
             providerReward = totalReward / providerState.rewardFraction;
             reward = totalReward - providerReward;
         }
-    }
-
-    // If provider has stopped providing, we want to subtract "accumulated token days" from that point.
-    function estimateAccumulatedFromTo(address account, uint128 from, uint128 to) private view returns (uint128 accumulated) {
-        uint128 prevTimepoint;
-        uint128 deltaTime;
-        DelegationState storage userDelegation = delegations[account];
-        if (userDelegation.delegationChanges.length > 1) {
-            for (uint256 i = 1; i < delegations[account].delegationChanges.length; i++) {
-                if (userDelegation.delegationChanges[i].timePoint > to) break;
-                if (userDelegation.delegationChanges[i].timePoint > from) {
-                    prevTimepoint = (from > userDelegation.delegationChanges[i - 1].timePoint)
-                        ? from
-                        : userDelegation.delegationChanges[i - 1].timePoint;
-
-                    deltaTime = userDelegation.delegationChanges[i].timePoint - prevTimepoint;
-                    accumulated += deltaTime * userDelegation.delegationChanges[i - 1].balance;
-                }
-            }
-        }
-        prevTimepoint = (from > userDelegation.delegationChanges[userDelegation.delegationChanges.length - 1].timePoint)
-            ? from
-            : userDelegation.delegationChanges[userDelegation.delegationChanges.length - 1].timePoint;
-
-        deltaTime = to - prevTimepoint;
-        accumulated += deltaTime * userDelegation.delegationChanges[userDelegation.delegationChanges.length - 1].balance;
-        accumulated = accumulated / 86400;
     }
 
     function claimYield(address account) public {
---
+++ contracts/ChromiaDelegationSync.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+
+/*
+    This contract has three parts: Delegation, Yield and TwoWeeksNoticeProvider.
+*/
+
+pragma solidity ^0.8.17;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import './TwoWeeksNoticeProvider.sol';
+import 'hardhat/console.sol';
+
+interface TwoWeeksNotice {
+    function estimateAccumulated(address account) external view returns (uint128, uint128);
+
+    function getStakeState(address account) external view returns (uint64, uint64, uint64, uint64);
+}
+
+contract ChromiaDelegationSync {
+    TwoWeeksNotice public twn;
+
+    struct DelegationChange {
+        uint128 timePoint;
+        uint128 balance;
+        address delegatedTo;
+    }
+
+    struct DelegationState {
+        uint128 processed;
+        uint128 processedDate;
+        DelegationChange[] delegationChanges;
+        mapping(uint32 => DelegationChange) delegationTimeline; // each uint key is a week starting from "startTime"
+    }
+
+    mapping(address => DelegationState) public delegations;
+
+    constructor(TwoWeeksNotice _twn) {
+        twn = _twn;
+    }
+
+    function verifyRemoteAccumulated(address account) internal view {
+        (uint128 remoteAccumulated, ) = twn.estimateAccumulated(account);
+        uint128 localAccumulated = estimateAccumulatedFromTo(
+            account,
+            delegations[account].processedDate,
+            uint128(block.timestamp)
+        );
+
+        require(
+            localAccumulated > (remoteAccumulated - delegations[account].processed) - 10,
+            'Accumulated doesnt match with TWN'
+        );
+        require(
+            localAccumulated < (remoteAccumulated - delegations[account].processed) + 10,
+            'Accumulated doesnt match with TWN'
+        );
+    }
+
+    // If provider has stopped providing, we want to subtract "accumulated token days" from that point.
+    function estimateAccumulatedFromTo(address account, uint128 from, uint128 to) private view returns (uint128 accumulated) {
+        uint128 prevTimepoint;
+        uint128 deltaTime;
+        DelegationState storage userDelegation = delegations[account];
+        if (userDelegation.delegationChanges.length > 1) {
+            for (uint256 i = 1; i < delegations[account].delegationChanges.length; i++) {
+                if (userDelegation.delegationChanges[i].timePoint > to) break;
+                if (userDelegation.delegationChanges[i].timePoint > from) {
+                    prevTimepoint = (from > userDelegation.delegationChanges[i - 1].timePoint)
+                        ? from
+                        : userDelegation.delegationChanges[i - 1].timePoint;
+
+                    deltaTime = userDelegation.delegationChanges[i].timePoint - prevTimepoint;
+                    accumulated += deltaTime * userDelegation.delegationChanges[i - 1].balance;
+                }
+            }
+        }
+        prevTimepoint = (from > userDelegation.delegationChanges[userDelegation.delegationChanges.length - 1].timePoint)
+            ? from
+            : userDelegation.delegationChanges[userDelegation.delegationChanges.length - 1].timePoint;
+
+        deltaTime = to - prevTimepoint;
+        accumulated += deltaTime * userDelegation.delegationChanges[userDelegation.delegationChanges.length - 1].balance;
+        accumulated = accumulated / 86400;
+    }
+}
